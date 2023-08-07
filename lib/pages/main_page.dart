e7e1804928df05159519a278713da4873a8f7783
B1loliddin <biloliddinbotirov000@gmail.com>
2023-08-07 20:11:00 +0500

    Frame of project

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,9 +5,64 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Text('Data'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(width: 30, height: 30, color: Colors.red),
+                  Container(width: 30, height: 30, color: Colors.green),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 375,
+                    height: 150,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 375,
+                    height: 50,
+                    color: Colors.black54,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 375 / 4,
+                  height: 50,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 375 / 4,
+                  height: 50,
+                  color: Colors.lightGreen,
+                ),
+                Container(
+                  width: 375 / 4,
+                  height: 50,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 375 / 4,
+                  height: 50,
+                  color: Colors.lightGreen,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
